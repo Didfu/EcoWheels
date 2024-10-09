7af7695c04d1e3747917111c2e7c3524be565fb6
dhruvmahyavanshi <mahyavanshidhruv@gmail.com>
2024-10-10 02:57:54 +0530

    some polishing

--- main/forms.py
+++
@@ -1,5 +0,0 @@
-# forms.py
-from django import forms
-
-class ChatForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 3, 'placeholder': 'Type your message here...'}))
